9d074595ed55b23f7ea9640071d0a3d80c9510f1
OliviaL18 <oliviatloos@gmail.com>
2020-01-18 15:54:39 -0500

    index variables

--- script.js
+++ script.js
@@ -3,23 +3,29 @@ var passwordLength = document.querySelector("#length");
 var lengthBtn = document.querySelector("#length-btn");
 var passwordArea = document.querySelector("#password");
 
-var randomString = Math.random().toString(36).substring(2, 8); 
+var miniString;
 
-var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var lowerLetterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperLetterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialCharArray = ["!","#","$","%","&","(",")","*","+","-",".","<","=",">","?","@","[","`","^","_","{","|","}","~",":",";"];
+makeMiniString();
 
-var indexLetterChar = Math.floor(Math.random() * Math.floor(27));
-var indexNumber = Math.floor(Math.random() * Math.floor(11));
-
-var number = numberArray[indexLetterChar];
-var lowerLetter = lowerLetterArray[indexLetterChar];
-var upperLetter = upperLetterArray[indexLetterChar];
-var specialChar = specialCharArray[indexLetterChar];
-
-var miniString = lowerLetter + upperLetter + specialChar + number;
-	console.log(miniString);
+function makeMiniString(){
+	var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+	var lowerLetterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	var upperLetterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+	var specialCharArray = ["!","#","$","%","&","(",")","*","+","-",".","<","=",">","?","@","[","`","^","_","{","|","}","~",":",";"];
+	
+	var indexLetterChar = Math.floor(Math.random() * 27);
+		console.log(`Letter/Character index: ${indexLetterChar}`);
+	var indexNumber = Math.floor(Math.random() * 11);
+	console.log(`Letter/Character index: ${indexNumber}`)
+	
+	var number = numberArray[indexNumber];
+	var lowerLetter = lowerLetterArray[indexLetterChar];
+	var upperLetter = upperLetterArray[indexLetterChar];
+	var specialChar = specialCharArray[indexLetterChar];
+	
+	var miniString = lowerLetter + upperLetter + specialChar + number;
+		console.log(miniString);
+}
 
 var length;
 
