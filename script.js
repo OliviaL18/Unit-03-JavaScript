c16596c3f9c50ea1a10b4d441ef4553b6abb8b97
OliviaL18 <oliviatloos@gmail.com>
2020-01-18 16:17:41 -0500

    some global variable changes

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-var generateBtn = document.querySelector("#generate");
 var passwordLength = document.querySelector("#length");
-var lengthBtn = document.querySelector("#length-btn");
+var includeLowerCheck = document.querySelector("#includeLower");
+var includeUpperCheck = document.querySelector("#includeUpper");
+var includeNumberCheck = document.querySelector("#incluedNumber");
+var includeSpecialCheck = document.querySelector("#includeSpecial");
 var passwordArea = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
 
 var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerLetterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperLetterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialCharArray = ["!","#","$","%","&","(",")","*","+","-",".","<","=",">","?","@","[","`","^","_","{","|","}","~",":",";"];
-
-var indexLetterChar = Math.floor(Math.random() * 27);
-	console.log(`Letter/Character index: ${indexLetterChar}`);
-var indexNumber = Math.floor(Math.random() * 11);
-console.log(`Letter/Character index: ${indexNumber}`)
-
+var indexLetterChar = Math.floor(Math.random() * 26);
+var indexNumber = Math.floor(Math.random() * 10);
 var number = numberArray[indexNumber];
 var lowerLetter = lowerLetterArray[indexLetterChar];
 var upperLetter = upperLetterArray[indexLetterChar];
