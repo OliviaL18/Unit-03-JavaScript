9a8ae9894195955b3820af0360d8dbf6fc5b89f0
OliviaL18 <oliviatloos@gmail.com>
2020-01-18 15:45:29 -0500

    add number/lower letter arrays, indexes, variables

--- script.js
+++ script.js
@@ -4,13 +4,22 @@ var lengthBtn = document.querySelector("#length-btn");
 var passwordArea = document.querySelector("#password");
 
 var randomString = Math.random().toString(36).substring(2, 8); 
-var capLetterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var lowerLetterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperLetterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var specialCharArray = ["!","#","$","%","&","(",")","*","+","-",".","<","=",">","?","@","[","`","^","_","{","|","}","~",":",";"];
-var selectCapChar = Math.floor(Math.random() * Math.floor(27));
-var capLetter = capLetterArray[selectCapChar];
-var specialChar = specialCharArray[selectCapChar];
-var fullString = randomString + capLetter + specialChar;
-	console.log(fullString);
+
+var indexLetterChar = Math.floor(Math.random() * Math.floor(27));
+var indexNumber = Math.floor(Math.random() * Math.floor(11));
+
+var number = numberArray[indexLetterChar];
+var lowerLetter = lowerLetterArray[indexLetterChar];
+var upperLetter = upperLetterArray[indexLetterChar];
+var specialChar = specialCharArray[indexLetterChar];
+
+var miniString = lowerLetter + upperLetter + specialChar + number;
+	console.log(miniString);
 
 var length;
 
@@ -18,12 +27,12 @@ generateBtn.addEventListener("click", generatePasword);
 
 function generatePasword(){
 	event.preventDefault();
-	var input = passwordLength.value;
-	if (isNaN(input) || input<8 || input>128){
+	var lengthInput = passwordLength.value;
+	if (isNaN(lengthInput) || lengthInput<8 || lengthInput>128){
 		alert("Please enter a number between 8 and 128.")
 	} else {
-	var length = parseInt(input);
-	passwordArea.innerHTML = fullString;
+	var length = parseInt(lengthInput);
+	passwordArea.innerHTML = miniString;
 	}
 }
 
